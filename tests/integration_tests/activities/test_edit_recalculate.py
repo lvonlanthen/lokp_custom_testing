c53b9104b0e05e4ed2a879a8c67f458b30fe534c
lvonlanthen <lukas.vonlanthen@gmail.com>
2014-08-31 14:14:51 +0200

    Started with unit tests

--- tests/integration_tests/activities/test_edit_recalculate.py
+++ tests/integration_tests/activities/test_edit_recalculate.py
@@ -28,7 +28,168 @@ class ActivityEditRecalculateTests(LmkpTestCase):
     def setUp(self):
         self.login()
 
-    @pytest.mark.test
+    # @pytest.mark.test
+    def test_blublu(self):
+        sh_uid = self.create('sh', get_new_diff(201), return_uid=True)
+        self.review('sh', sh_uid)
+        inv_data = [{
+            'id': sh_uid,
+            'version': 1,
+            'role': 1
+        }]
+        a_uid = self.create(
+            'a', get_new_diff(103, data=inv_data), return_uid=True)
+        self.review('a', a_uid)
+
+        self.create('a', get_edit_diff(101, a_uid, version=1))
+        self.create('a', get_edit_diff(106, a_uid, version=1))
+
+        self.review('a', a_uid, version=2)
+        self.review('a', a_uid, version=3)
+
+        res = self.read_one('a', a_uid, 'json')
+        self.assertEqual(res['total'], 4)
+        self.assertEqual(STATUS_ACTIVE, get_status_from_item_json(res, 0))
+        self.assertEqual(STATUS_EDITED, get_status_from_item_json(res, 1))
+        self.assertEqual(STATUS_INACTIVE, get_status_from_item_json(res, 2))
+        self.assertEqual(STATUS_INACTIVE, get_status_from_item_json(res, 3))
+
+        taggroups_v4 = res['data'][0]['taggroups']
+        self.assertEqual(len(taggroups_v4), 3)
+        # self.assertTrue(find_key_value_in_taggroups_json(
+        #     taggroups_v4, '[A] Checkbox 1', value='[A] Value D1',
+        #     main_tag=True))
+        inv_v4 = get_involvements_from_item_json(res, 0)
+        self.assertEqual(len(inv_v4), 1)
+
+    def test_recalculate_1(self):
+        sh_uid = self.create('sh', get_new_diff(201), return_uid=True)
+        self.review('sh', sh_uid)
+        a_uid = self.create('a', get_new_diff(101), return_uid=True)
+        self.review('a', a_uid)
+        self.create('a', get_edit_diff(101, a_uid, version=1))
+        inv_data = [{
+            'id': sh_uid,
+            'version': 1,
+            'role': 1
+        }]
+        self.create('a', get_edit_diff(102, a_uid, version=1, data=inv_data))
+
+        res = self.read_one('a', a_uid, 'json')
+        self.assertEqual(res['total'], 3)
+        self.assertEqual(STATUS_PENDING, get_status_from_item_json(res, 0))
+        self.assertEqual(STATUS_PENDING, get_status_from_item_json(res, 1))
+        self.assertEqual(STATUS_ACTIVE, get_status_from_item_json(res, 2))
+        taggroups_v3 = res['data'][0]['taggroups']
+        taggroups_v2 = res['data'][1]['taggroups']
+        taggroups_v1 = res['data'][2]['taggroups']
+        self.assertEqual(len(taggroups_v1), 2)
+        self.assertEqual(len(taggroups_v2), 3)
+        self.assertEqual(len(taggroups_v3), 2)
+        self.assertFalse(find_key_value_in_taggroups_json(
+            taggroups_v1, '[A] Checkbox 1', value='[A] Value D1',
+            main_tag=True))
+        self.assertTrue(find_key_value_in_taggroups_json(
+            taggroups_v2, '[A] Checkbox 1', value='[A] Value D1',
+            main_tag=True))
+        self.assertFalse(find_key_value_in_taggroups_json(
+            taggroups_v3, '[A] Checkbox 1', value='[A] Value D1',
+            main_tag=True))
+        inv_v3 = get_involvements_from_item_json(res, 0)
+        inv_v2 = get_involvements_from_item_json(res, 1)
+        inv_v1 = get_involvements_from_item_json(res, 2)
+        self.assertEqual(len(inv_v1), 0)
+        self.assertEqual(len(inv_v2), 0)
+        self.assertEqual(len(inv_v3), 1)
+        self.assertEqual(get_version_from_involvement_json(inv_v3), 2)
+
+        res = self.read_one('sh', sh_uid, 'json')
+        self.assertEqual(res['total'], 2)
+        self.assertEqual(STATUS_PENDING, get_status_from_item_json(res, 0))
+        self.assertEqual(STATUS_ACTIVE, get_status_from_item_json(res, 1))
+        inv_v2 = get_involvements_from_item_json(res, 0)
+        self.assertEqual(len(inv_v2), 1)
+        self.assertEqual(get_version_from_involvement_json(inv_v2), 3)
+
+    # @pytest.mark.test
+    def test_recalculate_1_review(self):
+        sh_uid = self.create('sh', get_new_diff(201), return_uid=True)
+        self.review('sh', sh_uid)
+        a_uid = self.create('a', get_new_diff(101), return_uid=True)
+        self.review('a', a_uid)
+        self.create('a', get_edit_diff(101, a_uid, version=1))
+        inv_data = [{
+            'id': sh_uid,
+            'version': 1,
+            'role': 1
+        }]
+        self.create('a', get_edit_diff(102, a_uid, version=1, data=inv_data))
+        self.review('a', a_uid, version=2)
+        self.review('a', a_uid, version=3)
+
+        res = self.read_one('a', a_uid, 'json')
+        self.assertEqual(res['total'], 4)
+        self.assertEqual(STATUS_ACTIVE, get_status_from_item_json(res, 0))
+        self.assertEqual(STATUS_EDITED, get_status_from_item_json(res, 1))
+        self.assertEqual(STATUS_INACTIVE, get_status_from_item_json(res, 2))
+        self.assertEqual(STATUS_INACTIVE, get_status_from_item_json(res, 3))
+
+        taggroups_v4 = res['data'][0]['taggroups']
+        self.assertEqual(len(taggroups_v4), 3)
+        self.assertTrue(find_key_value_in_taggroups_json(
+            taggroups_v4, '[A] Checkbox 1', value='[A] Value D1',
+            main_tag=True))
+        inv_v4 = get_involvements_from_item_json(res, 0)
+        self.assertEqual(len(inv_v4), 1)
+        self.assertEqual(get_version_from_involvement_json(inv_v4), 2)
+
+        res = self.read_one('sh', sh_uid, 'json')
+        self.assertEqual(res['total'], 2)
+        self.assertEqual(STATUS_ACTIVE, get_status_from_item_json(res, 0))
+        self.assertEqual(STATUS_INACTIVE, get_status_from_item_json(res, 1))
+        inv_v2 = get_involvements_from_item_json(res, 0)
+        self.assertEqual(len(inv_v2), 1)
+        self.assertEqual(get_version_from_involvement_json(inv_v2), 4)
+
+    def test_recalculate_1_review_2(self):
+        sh_uid = self.create('sh', get_new_diff(201), return_uid=True)
+        self.review('sh', sh_uid)
+        a_uid = self.create('a', get_new_diff(101), return_uid=True)
+        self.review('a', a_uid)
+        self.create('a', get_edit_diff(101, a_uid, version=1))
+        inv_data = [{
+            'id': sh_uid,
+            'version': 1,
+            'role': 1
+        }]
+        self.create('a', get_edit_diff(102, a_uid, version=1, data=inv_data))
+        self.review('a', a_uid, version=3)
+        self.review('a', a_uid, version=2)
+
+        res = self.read_one('a', a_uid, 'json')
+        self.assertEqual(res['total'], 4)
+        self.assertEqual(STATUS_ACTIVE, get_status_from_item_json(res, 0))
+        self.assertEqual(STATUS_INACTIVE, get_status_from_item_json(res, 1))
+        self.assertEqual(STATUS_EDITED, get_status_from_item_json(res, 2))
+        self.assertEqual(STATUS_INACTIVE, get_status_from_item_json(res, 3))
+
+        taggroups_v4 = res['data'][0]['taggroups']
+        self.assertEqual(len(taggroups_v4), 3)
+        self.assertTrue(find_key_value_in_taggroups_json(
+            taggroups_v4, '[A] Checkbox 1', value='[A] Value D1',
+            main_tag=True))
+        inv_v4 = get_involvements_from_item_json(res, 0)
+        self.assertEqual(len(inv_v4), 1)
+        self.assertEqual(get_version_from_involvement_json(inv_v4), 2)
+
+        res = self.read_one('sh', sh_uid, 'json')
+        self.assertEqual(res['total'], 2)
+        self.assertEqual(STATUS_ACTIVE, get_status_from_item_json(res, 0))
+        self.assertEqual(STATUS_INACTIVE, get_status_from_item_json(res, 1))
+        inv_v2 = get_involvements_from_item_json(res, 0)
+        self.assertEqual(len(inv_v2), 1)
+        self.assertEqual(get_version_from_involvement_json(inv_v2), 4)
+
     def test_recalculate_xxxxxxxxxxxxxxxxxxx(self):
         uid = self.create('a', get_new_diff(101), return_uid=True)
         self.review('a', uid)
@@ -74,7 +235,6 @@ class ActivityEditRecalculateTests(LmkpTestCase):
             taggroups_v3, '[A] Dropdown 1', value='[A] Value A2',
             main_tag=True))
 
-    @pytest.mark.test
     def test_recalculate_yyyyyyyyyyyyyyyyyyyy(self):
         uid = self.create('a', get_new_diff(101), return_uid=True)
         self.review('a', uid)
---
+++ tests/unit_tests/test_views.py
@@ -0,0 +1,27 @@
+import pytest
+from mock import Mock
+
+from ..integration_tests.base import (
+    LmkpTestCase,
+)
+from lmkp.views.views import (
+    get_output_format,
+)
+
+
+@pytest.mark.test
+@pytest.mark.usefixtures('app')
+@pytest.mark.unittest
+class TestViews(LmkpTestCase):
+
+    def test_get_output_format_returns_json_by_default(self):
+        mock_request = Mock()
+        mock_request.matchdict = {}
+        output = get_output_format(mock_request)
+        self.assertEqual(output, 'json')
+
+    def test_get_output_format_returns_output_format(self):
+        mock_request = Mock()
+        mock_request.matchdict = {'output': 'foo'}
+        output = get_output_format(mock_request)
+        self.assertEqual(output, 'foo')
